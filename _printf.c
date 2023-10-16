91dec9eaaa0f5852adcf16ec6412c579f565692e
hisreal123 <omosgoerge@gmail.com>
2023-10-16 20:50:14 +0100

    d

--- _printf.c
+++ _printf.c
@@ -2,21 +2,26 @@
 #include "main.h"
 /**
 * _printf - Printf function
-* @format: format.
-* Return: Printed chars.
+* @format: format
+* Return: Printed chars
 */
 int _printf(const char *format, ...)
 {
-int numOfchar = 0;
-va_list argList;
-if(format == NULL)
-    return (-1);
+    int numOfchar = 0;
+    va_list argList;
+
+    if(format == NULL)
+        return (-1);
+
     va_start(argList, format);
+
     while (*format)
     {
         if(*format != '%')
         {
+
             write(1, format, 1);
+
             if(*format != '\n'){
                 numOfchar++;
             }
@@ -24,7 +29,9 @@ if(format == NULL)
         else
         {
             format++;
+
             print_dI(format, &numOfchar);
+
             if (*format == '\0')
                 break;
             if (*format == '%')
