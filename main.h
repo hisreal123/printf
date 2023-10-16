53434de389f3654b77994887d3fc22cad5393c15
hisreal123 <omosgoerge@gmail.com>
2023-10-16 22:29:03 +0100

    fix errors 2

--- main.h
+++ main.h
@@ -7,9 +7,9 @@
 
 
 int _printf(const char *format, ...);
-void print_char(va_list argList, int *numOfchar);
-void print_string(va_list argList, int *numOfchar);
-void print_percent(const char *format, int *numOfchar);
-void print(const char *format, int *numOfchar);
-void print_dI(const char *format, int *numOfchar);
+// void print_char(va_list argList, int *numOfchar);
+// void print_string(va_list argList, int *numOfchar);
+// void print_percent(const char *format, int *numOfchar);
+// void print(const char *format, int *numOfchar);
+// void print_dI(const char *format, int *numOfchar);
 #endif
