32810de67d69aedd766c8a770687db36290125db
hisreal123 <omosgoerge@gmail.com>
2023-10-16 22:37:44 +0100

    d

--- main.h
+++ main.h
@@ -4,12 +4,6 @@
 #include <string.h>
 #include <unistd.h>
 
-
-
 int _printf(const char *format, ...);
-void function_char(va_list argList, int *numOfchar);
-void function_string(va_list argList, int *numOfchar);
-void function_percent(const char *format, int *numOfchar);
-void function_print(const char *format, int *numOfchar);
-void function_dI(const char *format, int *numOfchar);
+
 #endif
