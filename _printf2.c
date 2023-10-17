0f6c4bc23f59e313408ec92d397a0e724cc2c94a
Void-Individual <otayomitv@gmail.com>
2023-10-17 12:58:49 -0700

    the real file is printf2

--- _printf2.c
+++ _printf2.c
@@ -37,40 +37,10 @@ void _itoa(int num, char *buffer)
 	buffer[i] = '\0';
 	}
 
-void conversion_spec(const char *format, va_list argList, int *numOfchar)
+void conversion_spec(char format, va_list argList, int *numOfchar)
 {
-	if (*format == '%')
-		numOfchar = numOfchar + write(1, format, 1);
-
-	if (*format == 'c')
-	{
-		char c = va_arg(argList, int);
-
-		numOfchar = numOfchar + write(1, &c, 1);
-	}
-
-	if (*format == 's')
-	{
-		char *st = va_arg(argList, char *);
-		int st_len = strlen(st);
-
-		numOfchar = numOfchar + write(1, st, st_len);
-	}
-
-	if (*format == 'd' || *format == 'i')
-	{
-		int num = va_arg(argList, int);
-		char buffer[12];
-		int len;
-
-		_itoa(num, buffer);
-		len = strlen(buffer);
-		write(1, buffer, len);
-		numOfchar += len;
-	}
-
+	
 }
-
 /**
  * _printf - Printf function
  * @format: format
@@ -96,10 +66,34 @@ int _printf(const char *format, ...)
 		{
 			format++;
 
-			if (*format != '\0')
-				conversion_spec(format, argList, &numOfchar);
-			else
+			if (*format == 'd' || *format == 'i')
+			{
+				int num = va_arg(argList, int);
+				char buffer[12];
+				int len;
+
+				_itoa(num, buffer);
+				len = strlen(buffer);
+				write(1, buffer, len);
+				numOfchar += len;
+			}
+			if (*format == '\0')
 				break;
+			if (*format == '%')
+				numOfchar += write(1, format, 1);
+			else if (*format == 'c')
+			{
+				char c = va_arg(argList, int);
+
+				numOfchar  += write(1, &c, 1);
+			}
+			else if (*format == 's')
+			{
+				char *st = va_arg(argList, char *);
+				int st_len = strlen(st);
+
+				numOfchar = numOfchar + write(1, st, st_len);
+			}
 		}
 			format++;
 	}
