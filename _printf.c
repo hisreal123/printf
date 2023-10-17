8f881063856e237830ad327e3e35171c15323088
Void-Individual <otayomitv@gmail.com>
2023-10-17 18:40:39 +0100

    functional

--- _printf.c
+++ _printf.c
@@ -1,6 +1,43 @@
 #include <stdio.h>
 #include "main.h"
-/**
+
+void _itoa(int num, char *buffer)
+{
+	int i = 0;
+	int is_neg = 0;
+	int left;
+	int right;
+
+	if (num < 0)
+	{
+		is_neg = 1;
+		num = -num;
+	}
+	do 
+	{
+		buffer[i++] = num % 10 + '0';
+		num /= 10;
+	}	while (num > 0);
+
+	if (is_neg)
+	{	buffer[i++] = '-';
+	}
+	
+	left = 0;
+	right = i -1;
+
+	while (left < right)
+	{
+		char temp = buffer[left];
+		buffer[left] = buffer[right];
+		buffer[right] = temp;
+		left++;
+		right--;
+	}
+	buffer[i] = '\0';
+	}
+    
+    /**
 * _printf - Printf function
 * @format: format
 * Return: Printed chars
@@ -12,7 +49,6 @@ int _printf(const char *format, ...)
 
     if(format == NULL)
         return (-1);
-
     va_start(argList, format);
 
     while (*format)
@@ -28,16 +64,30 @@ int _printf(const char *format, ...)
 
             if (*format == 'd' || *format == 'i')
             {
-                function_dI(argList, &numOfchar);
+                 int num = va_arg(argList, int);
+                char buffer[12];
+                int len;
+
+                _itoa(num, buffer);
+                len = strlen(buffer);
+                write(1, buffer, len);
+                numOfchar += len;
             }
             if (*format == '\0')
                 break;
             if (*format == '%')
-                function_percent(format, &numOfchar);
+                numOfchar += write(1, format, 1);
             else if (*format == 'c')
-                function_char(argList, &numOfchar);
+                {
+                    char c = va_arg(argList, int);
+                    numOfchar  += write(1, &c, 1);
+                }
             else if (*format == 's')
-                function_string(argList, &numOfchar);
+                {
+                    char *st = va_arg(argList, char *);
+                    int st_len = strlen(st);
+                    numOfchar = numOfchar + write(1, st, st_len);
+                }
         }
         format++;
     }
